d94c172632dd4c81d469247df5c30fb1fa259726
rneko26 <rneko2006@gmail.com>
2024-08-12 15:44:07 +0700

    feat: Implement HealthCheck rpc

--- server/api/my_service.go
+++ server/api/my_service.go
@@ -3,6 +3,8 @@ package api
 import (
 	"context"
 	pb "grpc-gateway-101/server/proto"
+
+	"google.golang.org/grpc/codes"
 )
 
 type myServer struct {
@@ -17,6 +19,17 @@ func (s myServer) Echo(ctx context.Context, req *pb.StringMessage) (*pb.StringMe
 
 	result := &pb.StringMessage{
 		Value: "Echoing....",
+	}
+
+	return result, nil
+
+}
+
+func (s *myServer) HealthCheck(ctx context.Context, req *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
+
+	result := &pb.HealthCheckResponse{
+		Code:    codes.OK.String(),
+		Message: "API is running!",
 	}
 
 	return result, nil
